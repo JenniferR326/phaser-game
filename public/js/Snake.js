53b0b0d109f339453207317b63bdd8f2b64fce80
JenniferR326 <jennarafael@gmail.com>
2021-05-02 13:41:21 -0400

    add snake movement

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -1,8 +1,49 @@
 export default class Snake {
   constructor(scene) {
     this.scene = scene;
-    this.scene.add.rectangle(10, 10, 16, 16, 0xff0000);
+    this.lastMoveTime = 0;
+    this.moveInterval = 500;
+    this.direction = Phaser.Math.Vector2.DOWN;
+    this.body = [];
+    this.body.push(
+      this.scene.add.rectangle(100, 100, 16, 16, 0xff0000).setOrigin(0)
+    );
+    this.body.push(
+      this.scene.add.rectangle(0, 0, 16, 16, 0x0000ff).setOrigin(0)
+    );
+    scene.input.keyboard.on('keydown', (e) => {
+      this.keydown(e);
+    });
   }
 
-  update(time) {}
+  keydown(event) {
+    console.log(event);
+    switch (event.keyCode) {
+      case 37: // Left
+        this.direction = Phaser.Math.Vector2.LEFT;
+        break;
+      case 38: // Up
+        this.direction = Phaser.Math.Vector2.UP;
+        break;
+      case 39: // Right
+        this.direction = Phaser.Math.Vector2.RIGHT;
+        break;
+      case 40: //Down
+        this.direction = Phaser.Math.Vector2.DOWN;
+        break;
+    }
+  }
+
+  update(time) {
+    if (time >= this.lastMoveTime + this.moveInterval) {
+      this.lastMoveTime = time;
+      this.move();
+    }
+  }
+  move() {
+    this.body[1].x = this.body[0].x;
+    this.body[1].y = this.body[0].y;
+    this.body[0].x += this.direction.x * 16;
+    this.body[0].y += this.direction.y * 16;
+  }
 }
