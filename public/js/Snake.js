da007848a21538cb3ba2cec7acfce44a4b4e734e
JenniferR326 <jennarafael@gmail.com>
2021-05-02 14:10:15 -0400

    add apple

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -3,21 +3,42 @@ export default class Snake {
     this.scene = scene;
     this.lastMoveTime = 0;
     this.moveInterval = 500;
+    this.tileSize = 16;
     this.direction = Phaser.Math.Vector2.DOWN;
     this.body = [];
     this.body.push(
-      this.scene.add.rectangle(100, 100, 16, 16, 0xff0000).setOrigin(0)
+      this.scene.add
+        .rectangle(
+          this.scene.game.config.width / 2,
+          this.scene.game.config.height / 2,
+          this.tileSize,
+          this.tileSize,
+          0xff0000
+        )
+        .setOrigin(0)
     );
-    this.body.push(
-      this.scene.add.rectangle(0, 0, 16, 16, 0x0000ff).setOrigin(0)
-    );
+    this.apple = this.scene.add
+      .rectangle(0, 0, this.tileSize, this.tileSize, 0x00ff00)
+      .setOrigin(0);
+    this.positionApple();
+
     scene.input.keyboard.on('keydown', (e) => {
       this.keydown(e);
     });
   }
 
+  positionApple() {
+    this.apple.x =
+      Math.floor(
+        (Math.random() * this.scene.game.config.width) / this.tileSize
+      ) * this.tileSize;
+    this.apple.y =
+      Math.floor(
+        (Math.random() * this.scene.game.config.height) / this.tileSize
+      ) * this.tileSize;
+  }
+
   keydown(event) {
-    console.log(event);
     switch (event.keyCode) {
       case 37: // Left
         this.direction = Phaser.Math.Vector2.LEFT;
@@ -41,8 +62,10 @@ export default class Snake {
     }
   }
   move() {
-    this.body[1].x = this.body[0].x;
-    this.body[1].y = this.body[0].y;
+    for (let i = this.body.length - 1; i > 0; i--) {
+      this.body[i].x = this.body[i - 1].x;
+      this.body[i].y = this.body[i - 1].y;
+    }
     this.body[0].x += this.direction.x * 16;
     this.body[0].y += this.direction.y * 16;
   }
